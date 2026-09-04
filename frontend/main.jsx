1c600418b45daa4ba25e8bd530472856b80ef405
KRISHNA KARTHIK <krishnakarthik370@gmail.com>
2026-09-04 14:19:05 +0530

    Phase-4: Industry Module

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { AuthGate } from './components/AuthGate.jsx'
 import StudentDashboard from './components/StudentDashboard.jsx'
 import TrainingDashboard from './components/TrainingDashboard.jsx'
+import IndustryDashboard from './components/IndustryDashboard.jsx'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<AuthGate><App /></AuthGate>} />
         <Route path="/student" element={<AuthGate><StudentDashboard /></AuthGate>} />
         <Route path="/training" element={<AuthGate><TrainingDashboard /></AuthGate>} />
+        <Route path="/industry" element={<AuthGate><IndustryDashboard /></AuthGate>} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
